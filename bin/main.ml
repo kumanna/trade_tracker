de30a20674c8073c367d981194067e40cce1b401
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-03-22 17:40:08 +0530

    Initial commit

---
+++ bin/main.ml
@@ -0,0 +1,15 @@
+open Core
+
+let list_filename f =
+  print_endline f
+
+let command =
+  Command.basic
+    ~summary: "Generate and maintain a database to track trades."
+    ~readme: (fun () -> "More information (TODO)")
+    (let open Command.Let_syntax in
+     let open Command.Param in
+     let%map filename = anon ("filename" %: string) in
+     fun () -> list_filename filename)
+
+let () = Command_unix.run ~version:"1.0" command
